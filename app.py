7ef1f19c35d086069cd3346259e19d1babddcf90
proyect8 <63924675+proyect8@users.noreply.github.com>
2024-09-26 00:30:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,18 @@ import ast
 st.title("Gen Programación")
 st.subheader("Escribe las condiciones de activación de los genes usando lógica de programación para convertir la célula madre en un Hepatocito")
 
-# Función para analizar las comparaciones de condiciones
+# Función para analizar el código y extraer las condiciones
 def analizar_codigo(codigo):
     try:
         # Parseamos el código ingresado
         tree = ast.parse(codigo)
         condiciones = {}
 
-        # Recorremos el árbol de sintaxis abstracta (AST) para analizar las comparaciones
+        # Recorremos el árbol de sintaxis abstracta (AST) para analizar las condiciones
         for node in ast.walk(tree):
-            if isinstance(node, ast.Compare):  # Para comparar las expresiones (condiciones)
-                variable = node.left.attr  # La variable de la condición (ej: gene_visiondiurna)
-                valor = ast.literal_eval(node.comparators[0])  # El valor booleano (ej: False)
+            if isinstance(node, ast.If):
+                variable = node.test.left.id  # La variable de la condición (ej: gene_visiondiurna)
+                valor = ast.literal_eval(node.test.comparators[0])  # El valor booleano (ej: False)
                 condiciones[variable] = valor
 
         return condiciones, None
@@ -40,17 +40,17 @@ def verificar_genes(condiciones):
 
 # Ejemplo de cómo debe ingresar el código el estudiante
 st.code("""
-self.gene_visiondiurna == False
-self.formacionorina == False
-self.gene_mantenimientoglucosa == True
-self.gene_liberacionacidosgrasos == True
-self.gene_sintesisproteinas == True
-self.gene_controlapoptosis == True
-self.gene_controlpresionarterial == False
+if gene_visiondiurna == False:
+elif formacionorina == False:
+elif gene_mantenimientoglucosa == True:
+elif gene_liberacionacidosgrasos == True:
+elif gene_sintesisproteinas == True:
+elif gene_controlapoptosis == True:
+elif gene_controlpresionarterial == False:
 """, language='python')
 
 # Entrada de código por parte del estudiante
-codigo_usuario = st.text_area("Escribe tu código aquí (solo condiciones):")
+codigo_usuario = st.text_area("Escribe tu código aquí:")
 
 # Botón para finalizar
 if st.button("Finalizar"):
